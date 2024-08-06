d0a2fa89cbe41353ab0f0504a56ddcdb0ecca12d
GYK2419 <roze0404@naver.com>
2024-08-06 09:39:48 +0900

    nuxt 초기 세팅

---
+++ nuxt.config.js
@@ -0,0 +1,45 @@
+export default {
+  target: 'static', // 정적 웹사이트임을 명시
+  router: {
+    base: '/gayeongbusiness24/' // github repository 이름 넣기
+  },
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'project',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+  ],
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
