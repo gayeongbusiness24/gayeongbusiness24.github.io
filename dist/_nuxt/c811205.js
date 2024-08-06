6121f70b2d243e694b696c70bcd6e9bd46a425fa
GYK2419 <roze0404@naver.com>
2024-08-06 09:57:42 +0900

    nuxt 초기 세팅

---
+++ dist/_nuxt/c811205.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{199:function(n,o,w){w(200),n.exports=w(201)}},[[199,5,1,6]]]);
---
+++ dist/_nuxt/static/1722904611/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fgayeongbusiness24\u002F_nuxt\u002Fstatic\u002F1722904611",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fgayeongbusiness24\u002F",assetsPath:"\u002Fgayeongbusiness24\u002F_nuxt\u002F",cdnURL:a}}}}(null));
